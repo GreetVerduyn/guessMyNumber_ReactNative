b2e305132abedae3d153321aa90bff105f4ca9b9
Greet Verduyn <greetverduyn@proximus.be>
2022-10-17 16:06:00 +0200

    basic setup

---
+++ components/PrimaryButton.js
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+
+function PrimaryButton(children){
+    return(
+        <View>
+            <Text>
+                {children}
+            </Text>
+        </View>
+    )
+}
+
+export default PrimaryButton;
